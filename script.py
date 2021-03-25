ffb6d7fbe0d151849a4b37900c216e073cb37a3c
Gabriel Orofino <g.orofino@icloud.com>
2021-03-25 12:59:46 -0300

    Removed \x00 from string for enhanced Excel interpretation.

--- script.py
+++ script.py
@@ -3,7 +3,9 @@ import pandas
 with open('text.txt', 'r') as f:
     text = f.readlines()
 
-find_string = '\x00\t\x00V\x00a\x00l\x00.\x00o\x00b\x00j\x00e\x00t\x00o'
+text = [x.replace('\x00', '') for x in text]
+
+find_string = '\tVal.objeto'
 indices = [i for i, x in enumerate(text) if x.startswith(find_string)]
 indices.append(len(text))
 
@@ -12,7 +14,7 @@ dataset_compilation = []
 for i in range(len(indices)-1):
 
     dataset = text[indices[i]:indices[i+1]]
-    dataset = [row for row in dataset if row != '\x00\n']
+    dataset = [row for row in dataset if row != '\n']
 
     data = [row.split('\t') for row in dataset]
     
@@ -20,11 +22,11 @@ for i in range(len(indices)-1):
         df = pandas.DataFrame(data[1:], columns=data[0])
         dataset_compilation.append(df)
     except:
-        print(f"Something went wrong.")
+        print("Something went wrong.")
 
 for index, dataframe in enumerate(dataset_compilation):
-    if '\x00' in dataframe:
-        dataset_compilation[index] = dataset_compilation[index].drop(columns=['\x00'])
+    if '' in dataframe:
+        dataset_compilation[index] = dataset_compilation[index].drop(columns=[''])
 
 complete_dataframe = pandas.concat(dataset_compilation)
 
